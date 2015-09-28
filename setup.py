a12edc7cc4cab189cc4c732d53fed50a0034184d
Matt Travi <matt@dwolla.com>
2015-09-28 16:00:31 -0500

    created cli executable

--- setup.py
+++ setup.py
@@ -6,8 +6,14 @@ setup(
     name='splunk-crowd-auth',
     url='https://github.com/Dwolla/splunk-crowd-auth.git',
     install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'splunk-crowd-auth = crowd_scripted:main'
+        ]
+    },
 
     classifiers=[
+        'Environment :: Console',
         "Development Status :: 4 - Beta",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: System :: Systems Administration :: Authentication/Directory"

845cadd83f9b1a9110005e79c8d355747f330876
Matt Travi <matt@dwolla.com>
2015-09-28 11:49:00 -0500

    added setup script to enable pip installation

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+dependencies = ['python-crowd']
+
+setup(
+    name='splunk-crowd-auth',
+    url='https://github.com/Dwolla/splunk-crowd-auth.git',
+
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Systems Administration :: Authentication/Directory"
+    ]
+)
